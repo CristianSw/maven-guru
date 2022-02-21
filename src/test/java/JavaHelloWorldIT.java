2b9cf02167ce6a40e1bfeffc745ddfa64edcb000
CristianSw <60692725+CristianSw@users.noreply.github.com>
2022-02-21 10:12:36 +0200

    Added support for running integration tests with maven-failsafe-plugin

---
+++ src/test/java/JavaHelloWorldIT.java
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.Test;
+
+public class JavaHelloWorldIT {
+    @Test
+    void myIntegrationTest(){
+        System.out.println("Integration test runs........");
+    }
+}
