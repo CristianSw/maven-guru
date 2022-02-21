63fc1f259cb67ef88f2807888dde7ee0c6a55153
CristianSw <60692725+CristianSw@users.noreply.github.com>
2022-02-21 09:51:29 +0200

    Added testng utilite to the project.

---
+++ src/test/java/JavaHelloWorldTestNG.java
@@ -0,0 +1,11 @@
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class JavaHelloWorldTestNG {
+
+    @Test
+    public void testJavaHelloWorld() {
+        System.out.println("hello");
+    }
+}
